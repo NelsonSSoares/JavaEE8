0fdf84804d6d6fa665c351a64904a863b307b8dc
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2022-05-25 13:23:13 -0300

    Validation and Constraints

--- hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
+++ hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
@@ -2,22 +2,34 @@
 package academy.learnprogramming.entity;
 
 import java.time.LocalDate;
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Entity
 public class Todo {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    
     private Long id;
-   
+    
+    @NotEmpty(message = "Task must be set")
+    @Size(min = 10 , message = "Task shold not be set less than 10 characteres")
     private String task;
+   
+    @NotNull(message = "Due must be set")
+    @FutureOrPresent(message = "Dude must be in the present or future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+    
     private boolean isCompleted;
     private LocalDate dateCompleted;
     private LocalDate dateCreated;
--- hello-todo/src/main/java/academy/learnprogramming/rest/TodoRest.java
+++ hello-todo/src/main/java/academy/learnprogramming/rest/TodoRest.java
@@ -12,6 +12,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -45,8 +46,19 @@ public class TodoRest {
     
     @Path("list")
     @GET
-    
     public List<Todo> getTodos(){
         return todoService.getTodos();
     }
+    
+    @Path("status")
+    @POST
+    public Response markAsComplete(@QueryParam("id") Long id){
+        
+        Todo todo = todoService.findTodoById(id);
+        todo.setIsCompleted(true);
+        todoService.updateTodo(todo);
+        
+        return Response.ok(todo).build();
+    }
+    
 }
