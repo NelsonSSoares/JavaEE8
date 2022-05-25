3c3a37d564837b00af4db2246b9cbe521e74d8a3
Nelson de Sousa Soares <nelsonssoares@ibm.com>
2022-05-25 12:55:13 -0300

    validation and constraint

---
+++ src/main/java/academy/learnprogramming/service/TodoService.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming.service;
+
+import academy.learnprogramming.entity.Todo;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
+public class TodoService {
+    
+    @PersistenceContext
+    EntityManager entityManager;
+    
+    public Todo createTodo(Todo todo){
+        entityManager.persist(todo);
+        return todo;
+    }
+    
+    public Todo updateTodo(Todo todo){
+        entityManager.merge(todo);
+        return todo;
+    }
+    
+    public Todo findTodoById(Long id){
+        return entityManager.find(Todo.class, id);
+    }
+    
+    public List<Todo> getTodos(){
+        return entityManager.createQuery("SELECT T FROM Todo t", Todo.class).getResultList();
+    }
+    
+}
+
